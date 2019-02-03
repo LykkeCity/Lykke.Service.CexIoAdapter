6b36b25297dd07d241e06fc15f081e6d021b62d1
tarurar <tarutin_andrew@mail.ru>
2019-02-03 15:48:23 +0200

    added integration testing

--- src/Lykke.Service.CexIoAdapter/Startup.cs
+++ src/Lykke.Service.CexIoAdapter/Startup.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
+using Autofac;
 using JetBrains.Annotations;
 using Lykke.Common.ExchangeAdapter.Server;
 using Lykke.Sdk;
+using Lykke.Service.CexIoAdapter.Modules;
 using Lykke.Service.CexIoAdapter.Services;
 using Lykke.Service.CexIoAdapter.Services.CexIo;
 using Lykke.Service.CexIoAdapter.Services.Settings;
 using Lykke.Service.CexIoAdapter.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Nexogen.Libraries.Metrics.Prometheus.AspCore;
 
@@ -42,6 +46,25 @@ namespace Lykke.Service.CexIoAdapter
         }
 
         [UsedImplicitly]
+        public void ConfigureTestServices(IServiceCollection services)
+        {
+            services.BuildServiceProvider<AppSettings>(options =>
+            {
+                options.Logs = logs =>
+                {
+                    logs.UseEmptyLogging();
+                };
+
+                options.Extend = (collection, manager) =>
+                {
+                    collection.AddSingleton(manager.CurrentValue.CexIoAdapterService);
+                };
+
+                options.SwaggerOptions = new LykkeSwaggerOptions { ApiTitle = "CexIoAdapterService Test" };
+            });
+        }
+
+        [UsedImplicitly]
         public void Configure(IApplicationBuilder app)
         {
             var settings = app.ApplicationServices.GetService<CexIoAdapterSettings>();
@@ -66,6 +89,26 @@ namespace Lykke.Service.CexIoAdapter
                         settings.OrderBooks.CurrencyMapping));
                     x.UseHandleBusinessExceptionsMiddleware();
                     x.UseMiddleware<CollectMetricsMiddleware>();
+                };
+            });
+        }
+
+        [UsedImplicitly]
+        public void ConfigureTest(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
+        {
+            var settings = app.ApplicationServices.GetService<CexIoAdapterSettings>();
+
+            XApiKeyAuthAttribute.Credentials =
+                settings.Clients.ToDictionary(x => x.InternalApiKey, x => (object)x);
+
+            app.UseLykkeConfiguration(options =>
+            {
+                options.WithMiddleware = x =>
+                {
+                    x.UseAuthenticationMiddleware(token => new CexIoRestClient(
+                        GetCredentials(settings, token),
+                        settings.OrderBooks.CurrencyMapping));
+                    x.UseHandleBusinessExceptionsMiddleware();
                 };
             });
         }
---
+++ tests/Lykke.Service.CexIoAdapter.Tests/CexioIntegrationTests.cs
@@ -0,0 +1,113 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Lykke.Common.ExchangeAdapter.Contracts;
+using Lykke.Common.ExchangeAdapter.SpotController.Records;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Lykke.Service.CexIoAdapter.Tests
+{
+    public class CexioIntegrationTests : IClassFixture<CexioIntegrationWebApplicationFactory>
+    {
+        private readonly CexioIntegrationWebApplicationFactory _factory;
+
+        public CexioIntegrationTests(CexioIntegrationWebApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task CanGetWalletBalances()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/spot/getWallets");
+
+            Assert.True(response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task CanGetInstruments()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/spot/getInstruments");
+
+            Assert.True(response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task CanGetLimitOrders()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/spot/GetLimitOrders");
+
+            Assert.True(response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task CanPlaceAndCancelLimitOrder()
+        {
+            var client = _factory.CreateClient();
+
+            var createOrderRequest = new LimitOrderRequest
+                {TradeType = TradeType.Buy, Instrument = "BTC:USD", Price = 1500m, Volume = 0.01m};
+
+            // Place new order
+            var createResponse = await client.PostAsync(
+                "/spot/createLimitOrder",
+                new StringContent(
+                    JsonConvert.SerializeObject(createOrderRequest),
+                    Encoding.UTF8,
+                    "application/json"));
+
+            Assert.True(createResponse.StatusCode == HttpStatusCode.OK);
+
+            var orderIdResponse = await createResponse.Content.ReadAsAsync<OrderIdResponse>();
+
+            var cancelOrderRequest = new CancelLimitOrderRequest {OrderId = orderIdResponse.OrderId};
+
+            // Cancel order
+            var cancelResponse = await client.PostAsync(
+                "/spot/cancelOrder",
+                new StringContent(
+                    JsonConvert.SerializeObject(cancelOrderRequest),
+                    Encoding.UTF8,
+                    "application/json"));
+
+            Assert.True(cancelResponse.StatusCode == HttpStatusCode.OK);
+
+            var cancelOrderResponse = await cancelResponse.Content.ReadAsAsync<CancelLimitOrderResponse>();
+
+            Assert.Equal(cancelOrderResponse.OrderId, orderIdResponse.OrderId);
+
+            // Cancel same order again, should return 200 OK
+            var cancelSubsequentResponse = await client.PostAsync(
+                "/spot/cancelOrder",
+                new StringContent(
+                    JsonConvert.SerializeObject(cancelOrderRequest),
+                    Encoding.UTF8,
+                    "application/json"));
+
+            Assert.True(cancelSubsequentResponse.StatusCode == HttpStatusCode.OK);
+
+            var cancelOrderSubsequentResponse =
+                await cancelSubsequentResponse.Content.ReadAsAsync<CancelLimitOrderResponse>();
+
+            Assert.Equal(cancelOrderSubsequentResponse.OrderId, orderIdResponse.OrderId);
+
+            // Get previously created and cancelled order status
+            var statusResponse = await client.GetAsync($"/spot/limitOrderStatus?orderId={orderIdResponse.OrderId}");
+
+            Assert.True(statusResponse.StatusCode == HttpStatusCode.OK);
+
+            var statusOrderResponse = await statusResponse.Content.ReadAsAsync<OrderModel>();
+
+            Assert.Equal(statusOrderResponse.Id, orderIdResponse.OrderId);
+            Assert.Equal(OrderStatus.Canceled, statusOrderResponse.ExecutionStatus);
+        }
+    }
+}
